1f7187457b7c60fd985fc01380bd7ffc8453b5ab
mohammad <smilycolon@gmail.com>
2022-01-13 11:23:35 +0330

    editing page added for main-pages

---
+++ src/pages/main-page/edit/[banner_id].tsx
@@ -0,0 +1,27 @@
+import { MainPage } from 'components';
+import { GetServerSideProps, NextPage } from 'next';
+import { getSingleBanner } from 'utils/api/REST/actions/banners/getSingleBanner';
+
+const SingleMainPage: NextPage = () => <MainPage />;
+export default SingleMainPage;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  if (context?.req?.cookies?.token) {
+    const banner = await getSingleBanner(context.query.banner_id! as string, context?.req?.cookies?.token as string);
+
+    return {
+      props: {
+        initialState: {
+          banner,
+        },
+      },
+    };
+  } else {
+    return {
+      props: {},
+      redirect: {
+        destination: '/auth/login',
+      },
+    };
+  }
+};
