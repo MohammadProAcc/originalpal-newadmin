01f43909c1098b85595b7ced944fc01d01831eba
mohammad <smilycolon@gmail.com>
2022-01-17 22:06:37 +0330

    Dashboard Page Fixed

--- src/components/PageComponents/Dashboard/DashboardPage.tsx
+++ src/components/PageComponents/Dashboard/DashboardPage.tsx
@@ -1,6 +1,7 @@
 import { Card as OCard, CardBody, CardHeader } from '@paljs/ui';
 import { useStore } from 'utils';
 import styled from 'styled-components';
+import Link from 'next/link';
 
 export const DashboardPage = () => {
   const { products, users, orders } = useStore((state: any) => ({
@@ -12,18 +13,37 @@ export const DashboardPage = () => {
   return (
     <div style={{ display: 'flex' }}>
       <Card status="Primary">
-        <CardHeader>تعداد کاربران</CardHeader>
-        <CardBody>{users?.table?.total ?? '?'}</CardBody>
+        <Link href="#">
+          <CardHeader>تعداد کاربران</CardHeader>
+        </Link>
+        <CardBody>
+          {users?.table?.total ?? '?'} -{' '}
+          <Link href="/#">
+            <a href="/#">مشاهده همه</a>
+          </Link>
+        </CardBody>
       </Card>
 
       <Card status="Info">
         <CardHeader>تعداد محصولات</CardHeader>
-        <CardBody>{products?.data?.total ?? '?'}</CardBody>
+        <CardBody>
+          {products?.data?.total ?? '?'} -{' '}
+          <Link href="/products">
+            <a href="/products">مشاهده همه</a>
+          </Link>
+        </CardBody>
       </Card>
 
       <Card status="Success">
-        <CardHeader>تعداد سفارشات</CardHeader>
-        <CardBody>{orders?.data?.total ?? '?'}</CardBody>
+        <Link href="/orders">
+          <CardHeader>تعداد سفارشات</CardHeader>
+        </Link>
+        <CardBody>
+          {orders?.data?.total ?? '?'} -{' '}
+          <Link href="/orders">
+            <a href="/orders">مشاهده همه</a>
+          </Link>
+        </CardBody>
       </Card>
     </div>
   );
