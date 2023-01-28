bdd1ca936f2463bf2efea2e306e6a8577b52ae31
navid solimanian <navid.solimanian@gmail.com>
2023-01-28 14:54:11 +0330

    fix UserDetails Section

--- src/components/PageComponents/Users/UserDetails.tsx
+++ src/components/PageComponents/Users/UserDetails.tsx
@@ -1,13 +1,13 @@
-import { Badge, Collapse } from "@mantine/core";
+import { Badge, Collapse, Divider, Flex, Space, Text } from "@mantine/core";
 import { Button, Card as _Card, CardBody, CardHeader } from "@paljs/ui";
 import { Form, InputGroup } from "components";
 import Layout from "Layouts";
 import Link from "next/link";
-import React, { useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import styled from "styled-components";
-import { IOrder, UserDetails } from "types";
+import { UserDetails } from "types";
 import { $_search_user_details, reqSucceed, translator } from "utils";
 
 // TODO: complete the component
@@ -15,10 +15,7 @@ export function UserDetailsPage() {
   const [loading, setLoading] = useState(false);
   const [result, setResult] = useState<any>();
 
-  const {
-    register,
-    handleSubmit,
-  } = useForm();
+  const { register, handleSubmit } = useForm();
 
   async function onSearch(form: any) {
     setLoading(true);
@@ -50,75 +47,47 @@ export function UserDetailsPage() {
                 {_user.id} - <strong>{_user.name}</strong> - {_user.phone}
               </CardHeader>
               <Card>
-                <CardHeader>
-                  سفارشات
-                </CardHeader>
+                <CardHeader>سفارشات</CardHeader>
                 <CardBody>
-                  {_user.orders.length > 0
-                    ? (
-                      _user.orders.map(_order => (
-                        <CollapseCard
-                          header={
-                            <>
-                              سفارش {_order.id}
-                              <Link href={`/orders/${_order.id}`} passHref>
-                                <a>
-                                  مشاهده
-                                </a>
-                              </Link>
-                            </>
-                          }
-                        >
-                          {
-                            Object.entries(_order).map(([field, value]) => (
-                              <Card>
-                                <CardHeader status="Success">{translator(field)}</CardHeader>
-                                <CardBody dangerouslySetInnerHTML={{ __html: value }}></CardBody>
-                              </Card>
-                            ))
-                          }
-                        </CollapseCard>
-                      )
-                      )
-                    )
-                    : <Badge color="red">سفارشی وجود ندارد </Badge>
-                  }
+                  {_user.orders.length > 0 ? (
+                    _user.orders.map((_order) => (
+                      <Flex>
+                        <Text>سفارش {_order.id}</Text>
+                        <Space mx="md" />
+                        <Link href={`/orders/${_order.id}`} passHref>
+                          <a target="_blank">
+                            <Badge color="cyan">مشاهده</Badge>
+                          </a>
+                        </Link>
+                      </Flex>
+                    ))
+                  ) : (
+                    <Badge color="red">سفارشی وجود ندارد </Badge>
+                  )}
                 </CardBody>
               </Card>
 
               <Card>
-                <CardHeader>
-                  آدرس ها
-                </CardHeader>
+                <CardHeader>آدرس ها</CardHeader>
                 <CardBody>
-                  {_user.addresses.length > 0
-                    ? (
-                      _user.addresses.map(_address =>
-                        // TODO: complete item item
-                        <CollapseCard 
-                          header={
-                            <>
-                              آدرس {_address.id}
-                              <Link href={`/addresses/${_address.id}`} passHref>
-                                <a>
-                                  مشاهده
-                                </a>
-                              </Link>
-                            </>
-                          }
-                        >
-                          {
-                            Object.entries(_address).map(([field, value]) => (
-                              <Card>
-                                <CardHeader status="Success">{translator(field)}</CardHeader>
-                                <CardBody>{String(value)}</CardBody>
-                              </Card>
-                            ))
-                          }
-                        </CollapseCard>)
-                    )
-                    : <Badge color="red">آدرسی وجود ندارد </Badge>
-                  }
+                  {_user.addresses.length > 0 ? (
+                    _user.addresses.map((_address, index, array) => (
+                      <>
+                        <Flex>
+                          <Text>نشانی {_address.id}</Text>
+                          <Space mx="md" />
+                          <Link href={`/addresses/${_address.id}`} passHref>
+                            <a target="_blank">
+                              <Badge color="cyan">مشاهده</Badge>
+                            </a>
+                          </Link>
+                        </Flex>
+                        {index < array.length - 1 && <Divider variant="dashed" my="md" />}
+                      </>
+                    ))
+                  ) : (
+                    <Badge color="red">آدرسی وجود ندارد</Badge>
+                  )}
                 </CardBody>
               </Card>
             </Card>
@@ -132,7 +101,7 @@ export function UserDetailsPage() {
 const UsersList = styled.ul`
   margin-top: 1rem;
   padding: 0;
-  
+
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
@@ -140,7 +109,7 @@ const UsersList = styled.ul`
 
 const UserItem = styled.li`
   padding-top: 0.5rem;
-  
+
   display: flex;
 
   strong {
@@ -154,28 +123,24 @@ const Card = styled(_Card)`
 `;
 
 const CollapseHeader = styled(CardHeader).attrs({
-  status: "Info"
+  status: "Info",
 })`
   color: white;
 
   &:hover {
     cursor: pointer;
   }
-`
+`;
 
-const CollapseCard = (props: { header: any, children: any }) => {
+const CollapseCard = (props: { header: any; children: any }) => {
   // TODO: complete order item
   const [open, setOpen] = useState(false);
   return (
     <Card>
-      <CollapseHeader onClick={() => setOpen(_state => !_state)} >
-        {props.header}
-      </CollapseHeader>
+      <CollapseHeader onClick={() => setOpen((_state) => !_state)}>{props.header}</CollapseHeader>
       <CardBody>
-        <Collapse in={open}>
-          {props.children}
-        </Collapse>
+        <Collapse in={open}>{props.children}</Collapse>
       </CardBody>
     </Card>
-  )
-}
+  );
+};
