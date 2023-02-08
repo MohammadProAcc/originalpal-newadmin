eb9e34f4d115a8a16f782c49095ac44dd6fc1941
navid solimanian <navid.solimanian@gmail.com>
2023-02-08 14:18:28 +0330

    fix

--- src/components/PageComponents/MainPageSection/SingleMainPageSectionPage.tsx
+++ src/components/PageComponents/MainPageSection/SingleMainPageSectionPage.tsx
@@ -15,26 +15,21 @@ import {
   getSingleMainPageSection,
   getTagsList,
   has,
-  preppend,
   removeItem,
   search_in,
   translator,
-  useStore,
   useUserStore,
 } from "utils";
 
 export const SingleMainPageSectionPage: React.FC = () => {
   const router = useRouter();
-  const { mainPageSection } = useStore((state: any) => ({
-    mainPageSection: state?.mainPageSection,
-  }));
   const permissions = useUserStore().getPermissions();
 
   const [itemToRemove, setItemToRemove] = useState<any>(null);
   const closeRemovalModal = () => setItemToRemove(false);
 
   const remove = async (removeId: any) => {
-    await removeItem("mainPageSection", removeId, deleteMainPageSection, () => router.push("/mainPageSection"), [
+    await removeItem("mainPageSection", removeId, deleteMainPageSection, () => router.push("/main-page-sections"), [
       `بخش صقحه اصلی ${removeId} با موفقیت حذف شد`,
       "حذف بخش صقحه اصلی موفقیت آمیز نبود",
     ]);
@@ -50,16 +45,16 @@ export const SingleMainPageSectionPage: React.FC = () => {
   );
 
   return (
-    <Layout title={`بخش صفحه اصلی ${mainPageSection?.id}`}>
+    <Layout title={`بخش صفحه اصلی ${mainPageSectionQuery.data?.id}`}>
       <h1 style={{ marginBottom: "4rem" }}>
-        مشاهده بخش صفحه اصلی {mainPageSection?.id}
+        مشاهده بخش صفحه اصلی {mainPageSectionQuery.data?.id}
         {has(permissions, PermissionEnum.editMainPageSection) && (
-          <HeaderButton status="Info" href={`/mainPageSection/edit/${mainPageSection?.id}`}>
+          <HeaderButton status="Info" href={`/main-page-sections/edit/${mainPageSectionQuery.data?.id}`}>
             ویرایش
           </HeaderButton>
         )}
         {has(permissions, PermissionEnum.deleteMainPageSection) && (
-          <HeaderButton status="Danger" onClick={() => setItemToRemove(mainPageSection)}>
+          <HeaderButton status="Danger" onClick={() => setItemToRemove(mainPageSectionQuery.data)}>
             حذف
           </HeaderButton>
         )}
@@ -135,7 +130,7 @@ export const SingleMainPageSectionPage: React.FC = () => {
       {/* ....:::::: Modals :::::.... */}
       <Modal on={itemToRemove} toggle={closeRemovalModal}>
         <ModalBox>
-          <div style={{ marginBottom: "1rem" }}>آیا از حذف بخش صقحه اصلی {itemToRemove?.id} اطمینان دارید؟</div>
+          <div style={{ marginBottom: "1rem" }}>آیا از حذف بخش صفحه اصلی {itemToRemove?.id} اطمینان دارید؟</div>
           <FlexContainer jc="space-between">
             <Button onClick={closeRemovalModal}>انصراف</Button>
             <Button onClick={() => remove(itemToRemove?.id)} status="Danger">
