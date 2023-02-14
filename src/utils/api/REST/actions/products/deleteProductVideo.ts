f66432c1dbf0ea444348185feda3d64e62a01dfd
mohammad javad <javadthedeveloper@gmail.com>
2023-02-14 14:45:26 +0330

    fix product's video deletion

--- src/utils/api/REST/actions/products/deleteProductVideo.ts
+++ src/utils/api/REST/actions/products/deleteProductVideo.ts
@@ -1,14 +1,12 @@
-import { admin } from 'utils'
+import { admin } from "utils";
 
 export const deleteProductVideo = async (product_id: string, u: string, token: string) => {
   try {
-    const { data } = await admin(token ?? '').post(`${process.env.API}/admin/products/${product_id}/video/delete`, {
-      data: {
-        u,
-      },
-    })
-    return data
+    const { data } = await admin(token ?? "").post(`${process.env.API}/admin/products/${product_id}/video/delete`, {
+      u,
+    });
+    return data;
   } catch (err) {
-    return err
+    return err;
   }
-}
+};
