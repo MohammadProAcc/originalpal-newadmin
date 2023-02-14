f91f78b26479f581655965a1e27796aeb7c1d040
mohammad javad <javadthedeveloper@gmail.com>
2023-02-14 17:00:09 +0330

    fix build issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,4 @@
-import {
-  Hydrate,
-  QueryClient,
-  QueryClientProvider
-} from "@tanstack/react-query";
+import { Hydrate, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AppProps } from "next/app";
 import NextNprogress from "nextjs-progressbar";
@@ -13,27 +9,23 @@ import "styles/global.css";
 import { useState } from "react";
 import { StoreProvider, useHydrate } from "utils";
 import { createEmotionCache, MantineProvider } from "@mantine/core";
-import rtlPlugin from "stylis-plugin-rtl"
+import rtlPlugin from "stylis-plugin-rtl";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  const store = useHydrate(pageProps.initialState);
+  const store = useHydrate((pageProps as any).initialState);
 
   const [queryClient] = useState(() => new QueryClient());
 
   const rtlCache = createEmotionCache({
-    key: 'mantine-rtl',
+    key: "mantine-rtl",
     stylisPlugins: [rtlPlugin],
   });
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
+      <Hydrate state={(pageProps as any).dehydratedState}>
         <StoreProvider store={store}>
-          <MantineProvider
-            withGlobalStyles
-            emotionCache={rtlCache}
-            theme={{ dir: "rtl" }}
-          >
+          <MantineProvider withGlobalStyles emotionCache={rtlCache} theme={{ dir: "rtl" }}>
             <ToastContainer />
             <NextNprogress />
             <Component {...pageProps} />
